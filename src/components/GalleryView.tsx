315adc03c19943326c669c7bd0e589f7396476d9
tulili <tulilirockz.pub+git@gmail.com>
2023-09-04 11:40:19 -0300

    feat: astro 3.0 + fix leading slashes

--- src/components/GalleryView.tsx
+++ src/components/GalleryView.tsx
@@ -2,11 +2,11 @@ import { useState } from "preact/hooks";
 import { v4 as uuidv4 } from 'uuid';
 
 const close_gallery = (id: string) => {
-    document.getElementById(id)!.classList.add("hidden");
+    document.getElementById(id)?.classList.add("hidden");
 }
 
 const show_gallery = (id: string) => {
-    document.getElementById(id)!.classList.remove("hidden");
+    document.getElementById(id)?.classList.remove("hidden");
 }
 
 type GalleryItem = {
@@ -30,16 +30,16 @@ export default function GalleryView(props: { product_type: string, gallery_items
                         <a onClick={() => {
                             document.getElementById(id_gallery_viewer)?.requestFullscreen();
                         }}>
-                            <img src={website_base_url + "/icon/mdi-fullscreen.svg"} alt="Tela Cheia" class="h-12 w-12 lg:h-16 lg:w-16 text-white" id={id_fullscreen_gallery_photo} />
+                            <img src={`${website_base_url}/icon/mdi-fullscreen.svg`} alt="Tela Cheia" class="h-12 w-12 lg:h-16 lg:w-16 text-white" id={id_fullscreen_gallery_photo} />
                         </a>
                         <a onClick={() => close_gallery(id_gallery_viewer)}>
-                            <img src={website_base_url + "/icon/mdi-close-circle.svg"} alt="Fechar" class="h-12 w-12 lg:h-16 lg:w-16 text-white" />
+                            <img src={`${website_base_url}/icon/mdi-close-circle.svg`} alt="Fechar" class="h-12 w-12 lg:h-16 lg:w-16 text-white" />
                         </a>
                     </div>
                 </div>
                 <div class="flex flex-row justify-center items-center" style="height: 70vh;">
                     <div class="flex flex-col justify-center items-center bg-white max-w-md rounded-xl p-10">
-                        <img src={website_base_url + "/gallery/" + selected_image} alt={current_item.title} class="scale-110" />
+                        <img src={`${website_base_url}/gallery/${selected_image}`} alt={current_item.title} class="scale-110" />
                         <p class="text-xl lg:text-2xl py-3 mt-3">{current_item.title}</p>
                         <p class="text-md lg:text-lg text-left">{current_item.description}</p>
                     </div>
@@ -47,13 +47,13 @@ export default function GalleryView(props: { product_type: string, gallery_items
             </div>
             {props.gallery_items.map(item =>
                 <img
-                    id={props.product_type + "/" + item.url}
+                    id={`${props.product_type}/${item.url}`}
                     onClick={() => {
-                        set_selected_image(props.product_type + "/" + item.url);
+                        set_selected_image(`${props.product_type}/${item.url}`);
                         set_current_item(item);
                         show_gallery(id_gallery_viewer);
                     }}
-                    src={website_base_url + "/gallery/" + props.product_type + "/" + item.url}
+                    src={`${website_base_url}/gallery/${props.product_type}/${item.url}`}
                     alt={item.title}
                     decoding="async"
                     loading="lazy"
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -47,17 +47,17 @@ export default function Navbar() {
                         class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow rounded-box w-52 bg-white text-gray-800"
                     >
                         <li><a href={base_url}>Início</a></li>
-                        <li><a href={base_url + "galeria/toldos"}>Toldos</a></li>
-                        <li><a href={base_url + "galeria/coberturas"}>Coberturas</a></li>
-                        <li><a href={base_url + "galeria/telas"}>Telas</a></li>
-                        <li><a href={base_url + "galeria/vidros"}>Vidros</a></li>
-                        <li><a href={base_url + "galeria/cortinas"}>Cortinas</a></li>
-                        <li><a href={base_url + "#contato"}>Contato</a></li>
+                        <li><a href={`${base_url}/galeria/toldos`}>Toldos</a></li>
+                        <li><a href={`${base_url}/galeria/coberturas`}>Coberturas</a></li>
+                        <li><a href={`${base_url}/galeria/telas`}>Telas</a></li>
+                        <li><a href={`${base_url}/galeria/vidros`}>Vidros</a></li>
+                        <li><a href={`${base_url}/galeria/cortinas`}>Cortinas</a></li>
+                        <li><a href={`${base_url}/#contato`}>Contato</a></li>
                     </ul>
                 </div>
                 <a class="btn btn-ghost normal-case text-xl hidden lg:block" href={base_url}
                 ><img
-                        src={base_url + "logo_slim.png"}
+                        src={`${base_url}/logo_slim.png`}
                         alt="Logo ToldosMania"
                         loading="lazy"
                         decoding="async"
@@ -68,21 +68,21 @@ export default function Navbar() {
             <div class="navbar-center hidden lg:flex">
                 <ul class="menu menu-horizontal px-1 xl:text-lg text-gray-800">
                     <li><a href={base_url}>Início</a></li>
-                    <li><a href={base_url + "galeria/toldos"}>Toldos</a></li>
-                    <li><a href={base_url + "galeria/coberturas"}>Coberturas</a></li>
-                    <li><a href={base_url + "galeria/telas"}>Telas</a></li>
-                    <li><a href={base_url + "galeria/vidros"}>Vidros</a></li>
-                    <li><a href={base_url + "galeria/cortinas"}>Cortinas</a></li>
+                    <li><a href={`${base_url}/galeria/toldos`}>Toldos</a></li>
+                    <li><a href={`${base_url}/galeria/coberturas`}>Coberturas</a></li>
+                    <li><a href={`${base_url}/galeria/telas`}>Telas</a></li>
+                    <li><a href={`${base_url}/galeria/vidros`}>Vidros</a></li>
+                    <li><a href={`${base_url}/galeria/cortinas`}>Cortinas</a></li>
                 </ul>
             </div>
             <div class="navbar-end">
                 <a
-                    href={base_url + "#contato"}
+                    href={`${base_url}/#contato`}
                     class="btn btn-info text-white sm:btn-md border-b-4 border-blue-400 hover:bg-blue-400 hover:border-blue-400 hidden lg:flex"
                 >Contato</a>
                 <a class="btn btn-ghost normal-case text-xl lg:hidden" href={base_url}
                 ><img
-                        src={base_url + "logo.jpg"}
+                        src={`${base_url}/logo.jpg`}
                         alt="Logo ToldosMania"
                         class="object-scale-down h-12 w-32 rounded-xl border-4 bg-white border-white"
                         decoding="async"
