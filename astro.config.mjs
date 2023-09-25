cd544bd1e7e3ccca0a9934711a72e658d15e3884
tulili <tulilirockz.pub+git@gmail.com>
2023-09-25 17:34:51 -0300

    feat: image optimization on prod build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, squooshImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    service: squooshImageService(),
+  },
   integrations: [tailwind(), solidJs(
     {
       include: ['**/solid/*'],
