d1c657af0262f4084307c5d0c15173903d676f30
Tulili <tulilirockz@outlook.com>
2023-10-04 21:47:02 -0300

    feat: robots.txt and sitemap in build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,22 @@
 import { defineConfig, squooshImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
+import sitemap from '@astrojs/sitemap';
+import prefetch from '@astrojs/prefetch';
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   image: {
-    service: squooshImageService(),
+    service: squooshImageService()
   },
-  integrations: [tailwind(), solidJs(
-    {
-      include: ['**/solid/*'],
-    }
-  )],
+  integrations: [tailwind(), solidJs({
+    include: ['**/solid/*']
+  }), sitemap(), prefetch(), robotsTxt({
+      sitemap: [
+        'https://toldosmania.github.io/sitemap-index.xml',
+      ],
+    })],
   site: "https://toldosmania.github.io",
   base: "/Website"
 });
